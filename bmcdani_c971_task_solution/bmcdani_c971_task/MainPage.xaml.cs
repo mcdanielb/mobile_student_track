8f97ac885a38c731e6238d47abd9c618df7cd130
Bradley <mcdaniel.bradley@gmail.com>
2023-10-19 16:37:09 -0700

    removed constants & dbconnect classes, begin rebuilding db connection the simple way

--- bmcdani_c971_task_solution/bmcdani_c971_task/MainPage.xaml.cs
+++ bmcdani_c971_task_solution/bmcdani_c971_task/MainPage.xaml.cs
@@ -6,7 +6,8 @@ namespace bmcdani_c971_task
     public partial class MainPage : ContentPage
     {
         private List<Term> terms;
- 
+        private bool buttonClicked = false;
+
         public MainPage()
         {
             InitializeComponent();
@@ -44,10 +45,34 @@ namespace bmcdani_c971_task
             return conn.Table<Term>().ToList();
         }
 
-        private void TermButton_Clicked(object sender, EventArgs e)
+        private async void TermButton_Clicked(object sender, EventArgs e)
         {
+            if (buttonClicked) return;
+
+            buttonClicked = true;
+
             Button clickedButton = (Button)sender;
             int selectedTermId = (int)clickedButton.CommandParameter;
+
+            using (var connectionManager = new SQLiteConnManager<Term>("AcademicTrackerSQLite.db3"))
+            {
+                using (var conn = connectionManager.GetConnection())
+                {
+                    Term selectedTerm = conn.Table<Term>().FirstOrDefault(t => t.Id == selectedTermId);
+                    if (selectedTerm != null)
+                    {
+                        await Navigation.PushAsync(new CourseView(selectedTermId, selectedTerm.Name));
+                    }
+                }
+            }
+             
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            buttonClicked = false;
         }
 
         private void AddTermBtn_Clicked(object sender, EventArgs e)
--- bmcdani_c971_task_solution/bmcdani_c971_task/Pages/CourseView.xaml.cs
+++ bmcdani_c971_task_solution/bmcdani_c971_task/Pages/CourseView.xaml.cs
@@ -5,39 +5,46 @@ namespace bmcdani_c971_task.Pages;
 public partial class CourseView : ContentPage
 {
 	private List<Course> courses;
+    private int selectedTermId;
+    private string selectedTermName;
 
-	public CourseView(int selectedTermId)
+	public CourseView(int termId, string termName)
 	{
 		InitializeComponent();
 
-		using (var connectionManager = new SQLiteConnManager<Course>("AcademicTrackerSQLite.db3"))
-		{
-			using (var conn = connectionManager.GetConnection())
-			{
-				courses = GetCoursesForTerm(conn, selectedTermId);
-			}
-		}
+        TermTitleLbl.Text = termName;
+        selectedTermId = termId;
+        selectedTermName = termName;
 
 		UpdateCourses();
 	}
 
-	private List<Course> GetCoursesForTerm(SQLiteConnection conn, int termId)
-	{
-		return conn.Table<Course>().Where(c => c.TermId == termId).ToList();
-	}
-
 	private void UpdateCourses()
 	{
-		foreach (var course in courses)
-		{
-			Button courseButton = new Button
-			{
-				Text = course.Name,
-				FontSize = 18,
-				Margin = new Thickness(0, 5),
-				WidthRequest = 100
-			};
-            coursesStackLayout.Children.Add(courseButton);
+        using (var connectionManager = new SQLiteConnManager<Course>("AcademicTrackerSQLite.db3"))
+        {
+            using (var conn = connectionManager.GetConnection())
+            {
+                courses = GetCoursesForTerm(conn, selectedTermId);
+
+                foreach (var course in courses)
+                {
+                    Button courseButton = new Button
+                    {
+                        Text = course.Name,
+                        FontSize = 18,
+                        Margin = new Thickness(0, 5),
+                        WidthRequest = 200
+                    };
+                    coursesStackLayout.Children.Add(courseButton);
+                }
+            }
         }
+        
 	}
+
+    private List<Course> GetCoursesForTerm(SQLiteConnection conn, int termId)
+    {
+        return conn.Table<Course>().Where(c => c.TermId == termId).ToList();
+    }
 }
